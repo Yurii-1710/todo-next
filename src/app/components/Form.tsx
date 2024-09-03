9bbc44cae3fea4ff76ca6ad6d79d41ad24ed6567
Yurii Holovchenko <88655907+Yuriy1710@users.noreply.github.com>
2024-09-03 11:42:54 +0200

    added DB, input

---
+++ src/app/components/Form.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import React, { useRef } from 'react'
+import { formProps } from '../types'
+
+const Form = ({ children, action, className, onSubmit }: formProps) => {
+
+  const ref = useRef<HTMLFormElement>(null)
+
+  return (
+    <form action={async (formData) => {
+      await action(formData)
+      ref.current?.reset()
+    }}
+      onSubmit={onSubmit}
+      ref={ref}
+    >
+      {children}
+    </form>
+  )
+}
+
+export default Form
